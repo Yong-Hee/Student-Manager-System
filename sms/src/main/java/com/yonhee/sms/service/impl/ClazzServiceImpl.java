65d3860a281863f186c39c541252ba673eb9f643
Yong-Hee <550274236@qq.com>
2020-08-20 20:01:30 +0800

    “For all

---
+++ sms/src/main/java/com/yonhee/sms/service/impl/ClazzServiceImpl.java
@@ -0,0 +1,47 @@
+package com.yonhee.sms.service.impl;
+
+import com.yonhee.sms.bean.Clazz;
+import com.yonhee.sms.dao.ClazzMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.yonhee.sms.service.ClazzService;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ClazzServiceImpl implements ClazzService {
+
+    //注入Mapper接口对象
+    @Autowired
+    private ClazzMapper clazzMapper;
+
+    @Override
+    public List<Clazz> selectList(Clazz clazz) { return clazzMapper.selectList(clazz); }
+
+    @Override
+    public List<Clazz> selectAll() {
+        return clazzMapper.selectAll();
+    }
+
+    @Override
+    public Clazz findByName(String name) {
+        return clazzMapper.findByName(name);
+    }
+
+    @Override
+    public int insert(Clazz clazz) {
+        return clazzMapper.insert(clazz);
+    }
+
+    @Override
+    public int update(Clazz clazz) {
+        return clazzMapper.update(clazz);
+    }
+
+    @Override
+    public int deleteById(Integer[] ids) {
+        return clazzMapper.deleteById(ids);
+    }
+}
---
+++ sms/src/main/java/com/yonhee/sms/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,55 @@
+package com.yonhee.sms.service.impl;
+
+import com.yonhee.sms.bean.LoginForm;
+import com.yonhee.sms.bean.Teacher;
+import com.yonhee.sms.dao.TeacherMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.yonhee.sms.service.TeacherService;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class TeacherServiceImpl implements TeacherService {
+
+    //注入Mapper接口对象
+    @Autowired
+    private TeacherMapper teacherMapper;
+
+    @Override
+    public Teacher login(LoginForm loginForm) {
+        return teacherMapper.login(loginForm);
+    }
+
+    @Override
+    public List<Teacher> selectList(Teacher teacher) {
+        return teacherMapper.selectList(teacher);
+    }
+
+    @Override
+    public Teacher findByTno(Teacher teacher) {
+        return teacherMapper.findByTno(teacher);
+    }
+
+    @Override
+    public int insert(Teacher teacher) {
+        return teacherMapper.insert(teacher);
+    }
+
+    @Override
+    public int update(Teacher teacher) {
+        return teacherMapper.update(teacher);
+    }
+
+    @Override
+    public int updatePassowrd(Teacher teacher) {
+        return teacherMapper.updatePassword(teacher);
+    }
+
+    @Override
+    public int deleteById(Integer[] ids) {
+        return teacherMapper.deleteById(ids);
+    }
+}
